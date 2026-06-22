e26536ca7fb54c3513903617dfa344a7a9be1c2a
M-archand <13469995+M-archand@users.noreply.github.com>
2026-06-22 17:29:43 -0400

    Update version to 2.1.6

--- Plugin.cs
+++ Plugin.cs
@@ -40,7 +40,7 @@ namespace cs2_rockthevote
         ILogger<Plugin> logger) : BasePlugin, IPluginConfig<Config>
     {
         public override string ModuleName => "RockTheVote";
-        public override string ModuleVersion => "2.1.5";
+        public override string ModuleVersion => "2.1.6";
         public override string ModuleAuthor => "abnerfs, Marchand";
 
         private readonly DependencyManager<Plugin, Config> _dependencyManager = dependencyManager;
